18541d6acd95114bbef8513dc93ea4ce53b94185
sarvar-lapasov <sarvar_lapasov@mail.ru>
2023-12-11 03:58:42 +0500

    s:c

--- src/plugins/vuex/axios.js
+++ src/plugins/vuex/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import store from "@/plugins/vuex/store";
 
-axios.defaults.headers.common["Content-Type"] = "application/json";
-axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-axios.defaults.withCredentials = true;
+// axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+// axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+// axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use((config) => {
     if (config.url !== "http://localhost:8000/api/login") {
