2b366a410d3ca1985018c65ace4f227d9fb89e81
sarvar-lapasov <sarvar_lapasov@mail.ru>
2023-12-12 01:33:09 +0500

    s:c

--- src/plugins/vuex/axios.js
+++ src/plugins/vuex/axios.js
@@ -4,7 +4,7 @@ import store from "@/plugins/vuex/store";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 axios.interceptors.request.use((config) => {
     if (config.url !== "https://books.wuaze.com/api/login") {
