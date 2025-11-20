a3191625bca0edbfaa931f253b8b99aa3adedd56
sarvar_lapasov <66020178+sarvar-lapasov@users.noreply.github.com>
2025-11-20 20:27:17 +0500

    Update axios.js

--- src/plugins/vuex/axios.js
+++ src/plugins/vuex/axios.js
@@ -7,12 +7,12 @@ axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 axios.defaults.withCredentials = false;
 
 axios.interceptors.request.use((config) => {
-    if (config.url !== "https://bookapi.sarvarjon.uz/api/login") {
+    if (config.url !== "https://book.menuly.uz/api/login") {
         config.headers.Authorization = "Bearer " + store.getters.getToken;
     }
     return config;
 });
 
-axios.defaults.baseURL = "https://bookapi.sarvarjon.uz/api";
+axios.defaults.baseURL = "https://book.menuly.uz/api";
 
 export default axios;
